b95b8ae274ff7e902bce6fce55fb535f19dca08a
Rafael Hayashi <rafaelhayashi8@gmail.com>
2021-09-29 22:15:39 -0300

    [feat] criando arquivo style da pagina de login

---
+++ src/screens/Login/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const loginStyle = StyleSheet.create({
+  content: {
+    display: "flex",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    backgroundColor: "#3d3b3b"
+  },
+  view: {
+    width: "90%"
+  },
+  cardTitle: {
+    color: "#3d3b3b"
+  },
+  cardButton: {
+    margin: 2,
+    marginLeft: 8,
+    marginRight: 0
+  }
+})
